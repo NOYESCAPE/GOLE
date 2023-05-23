0c04bf4922af5029de8fed7114be3ce37ae0e226
wherehows <sensitive.hu@gmail.com>
2023-05-24 01:38:29 +0900

    chore: 테스트 코드 수정

--- apps/gole/components/Button/ExampleComponent.test.tsx
+++ apps/gole/components/Button/ExampleComponent.test.tsx
@@ -4,9 +4,12 @@ import { composeStories } from '@storybook/react';
 
 const { Info, Danger, Congrats, Documentation } = composeStories(stories);
 
-test('renders info banner with default args', () => {
-  const { getByText } = render(<Info />);
-  expect(getByText('정보')).toHaveStyle(
-    'background-color: rgba(224, 226, 255, 0.5)'
-  );
+describe('Banner', () => {
+  test('Should info banner have specific background color', () => {
+    const { getByText } = render(<Info />);
+
+    expect(getByText('정보')).toHaveStyle(
+      'background-color: rgba(224, 226, 255, 0.5)'
+    );
+  });
 });
